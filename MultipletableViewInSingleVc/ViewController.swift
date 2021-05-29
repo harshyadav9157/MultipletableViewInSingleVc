687be9e8a80e57124ef5513ffa5b67375536efd4
Harsh Yadav <harshyadav9157@users.noreply.github.com>
2021-05-29 16:29:34 +0530

    Initial Commit

---
+++ MultipletableViewInSingleVc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultipletableViewInSingleVc
+//
+//  Created by harsh yadav on 29/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
